c1c1bea2be75f0b3f0feac701c67c9173c16f38d
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2024-06-06 02:40:29 -0400

    Moved StrToNum.py to "Fundamentals"

---
+++ "Fundamentals"/StrToNum.py
@@ -0,0 +1,22 @@
+def string_to_number(s):
+    """
+    Args: 
+        s (string): A number that is taken as a string 
+                    ex: "4"
+                    
+    Returns: 
+        int: The string as an integer 
+             ex: "4" becomes 4
+
+    Examples:
+       >>>  string_to_number("4")
+       4
+       
+       >>> string_to_number("8")
+       8
+       
+       >>> string_to_number("26")
+       26
+    
+    """
+    return int(s)
